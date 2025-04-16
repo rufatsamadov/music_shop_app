1ab620a12464d9718096d48361d62ac0227136b9
rufatsamadov <rufat.samadov.a@asoiu.edu.az>
2025-04-16 18:44:41 +0400

    Cecenqqq

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def predict():
     return render_template('result.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(host=0.0.0.0,port=5001,debug=False,use_reloader=False)
+    app.run(host='0.0.0.0',port=5001,debug=False,use_reloader=False)
